d26bc65974e0fa9c2a7b46a189088b53715610f0
John <jmbalasta@gmail.com>
2022-04-02 21:27:31 -0700

    assignment finished

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const rs = require('readline-sync');
+var rs = require('readline-sync');
 
 //[x] prompt name question
 //[x] prompt fav hobby
@@ -7,32 +7,47 @@ const rs = require('readline-sync');
 // store list of users hobbies
 // print user hobby list aat the end
 
-const hobbies = [];
+var hobbies = [];
+var areYouSure = false;
+var moreHobbies = 0;
 
-const userName = rs.question('What is your name? ');
-const favHobby = rs.question(`${userName}, What is your favourite hobby? `);
+function getHobbyCount() {
+  moreHobbies = rs.questionInt(`How many other hobbies do you have? `);
+}
 
-hobbies.push(favHobby);
-
-const moreHobbies = rs.questionInt(`How many other hobbies do you have? `);
-
-if (moreHobbies > 0) {
-  let counter = 0;
-  while (counter < moreHobbies) {
-    let input = rs.prompt();
+function addHobbies(numHobbies) {
+  var counter = 0;
+  while (counter < numHobbies) {
+    var input = rs.prompt();
     hobbies.push(input);
     counter++;
 
-    if (counter < moreHobbies) {
+    if (counter < numHobbies) {
       console.log(
-        `-- Awesome! "${input}" is cool! What's the other ${moreHobbies - counter}? `
+        `-- Awesome! "${input}" is cool! What's the other ${numHobbies - counter}? `
       );
     } else {
       console.log(`I wish I could do ${hobbies} ...but I'm not even real`);
     }
   }
-  // add hobbies
-} else {
-  const areYouSure = rs.keyInYN(`Are you sure? ${hobbies[0]} is all you like to do? `);
 }
-console.log(hobbies);
+
+var userName = rs.question('What is your name? ');
+var favHobby = rs.question(`${userName}, What is your favourite hobby? `);
+
+hobbies.push(favHobby);
+getHobbyCount();
+
+if (moreHobbies > 0) {
+  console.log(`Cool, what are they?`);
+} else {
+  areYouSure = rs.keyInYN(`Are you sure? ${hobbies[0]} is all you like to do? `);
+}
+if (!areYouSure && hobbies.length <= 1) {
+  getHobbyCount();
+  console.log(`Cool, what are they?`);
+  addHobbies(moreHobbies);
+  //add hobbies
+} else if (areYouSure) {
+  console.log(`${hobbies} is cool, good for you.`);
+}
